2d7ccbd712d85a0f8ea65856a1969009f618e6bc
Clovis Fong <fongclovis@hotmail.com>
2022-08-23 10:10:08 +0800

    testing api

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleClick = () => {
+    fetch('https://api.etherscan.io/api?module=account&action=balance&address=0x55B3206EDF167f5C7c6c143f1B299409835A5777&tag=latest&apikey=F6FCNKMHH6SHM35Z3H399A1VDB9S3H24WA')
+      .then((response) => response.json())
+      .then((data) => setCount(data));
+  }
+
+
+  const deployLink = 'https://simplecrypto.vercel.app/'
+
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1>Simple Crypto</h1>
+      <h3>{count}</h3>
+      <button onClick={handleClick}>click</button>
     </div>
   )
 }
