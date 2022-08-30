93688f2e38bfe0eb6f728ab55d1a873379e5ca7e
Clovis Fong <fongclovis@hotmail.com>
2022-08-30 10:08:56 +0800

    sort and display top 5 NFT with longest holding period

--- src/components/NftHoldingPeriod.jsx
+++ src/components/NftHoldingPeriod.jsx
@@ -2,8 +2,7 @@ const NftHoldingPeriod = ({ defaultTx, address }) => {
 
     const today = Date.now() / 1000
 
-    const displayTimeDifference = (buyDate, sellDate) => {
-        const timeDifference = Number(sellDate) - Number(buyDate)
+    const displayTimeDifference = (timeDifference) => {
         const dayCount = Math.floor(timeDifference / (60 * 60 * 24))
 
         if (dayCount === 0) {
@@ -20,34 +19,48 @@ const NftHoldingPeriod = ({ defaultTx, address }) => {
     const inflowNft = defaultTx.filter((tx) => tx.from !== address.toLowerCase())
     const outflowNft = defaultTx.filter((tx) => tx.from === address.toLowerCase())
 
-    const found = outflowNft.find(element => inflowNft)
+    console.log(inflowNft)
 
-    const test = inflowNft.map((inflow) =>
+    // const nftStatusInfo = inflowNft.map((inflow) =>
+    //     outflowNft.some((outflow) => outflow.contractAddress === inflow.contractAddress && outflow.tokenID === inflow.tokenID) ?
+    //         ({ ['name']: inflow.tokenName, ['id']: inflow.tokenID, ['status']: 'Sold', ['holding_period']: displayTimeDifference(inflow.timeStamp, outflowNft.find(tx => tx.contractAddress === inflow.contractAddress && tx.tokenID === inflow.tokenID).timeStamp) }) :
+    //         ({ ['name']: inflow.tokenName, ['id']: inflow.tokenID, ['status']: 'Hold', ['holding_period']: displayTimeDifference(inflow.timeStamp, today) }))
+
+    const nftStatusInfo = inflowNft.map((inflow) =>
         outflowNft.some((outflow) => outflow.contractAddress === inflow.contractAddress && outflow.tokenID === inflow.tokenID) ?
-            ({ ['name']: inflow.tokenName, ['id']: inflow.tokenID, ['status']: 'Sold', ['holding-period']: displayTimeDifference(inflow.timeStamp, outflowNft.find(tx => tx.contractAddress === inflow.contractAddress && tx.tokenID === inflow.tokenID).timeStamp) }) :
-            ({ ['name']: inflow.tokenName, ['id']: inflow.tokenID, ['status']: 'Hold', ['holding-period']: displayTimeDifference(inflow.timeStamp, today) }))
+            ({ ['name']: inflow.tokenName, ['id']: inflow.tokenID, ['status']: 'Sold', ['holding_period']: Number(outflowNft.find(tx => tx.contractAddress === inflow.contractAddress && tx.tokenID === inflow.tokenID).timeStamp) - Number(inflow.timeStamp) }) :
+            ({ ['name']: inflow.tokenName, ['id']: inflow.tokenID, ['status']: 'Hold', ['holding_period']: Number(today) - Number(inflow.timeStamp) }))
 
 
-    console.log(test)
+    const sortHighToLow = nftStatusInfo.sort((a, b) => {
+        if (b.holding_period > a.holding_period) return 1;
+        if (b.holding_period < a.holding_period) return -1;
+        return 0;
+    })
+
+
+    const topFiveHoldingPeriod =
+        sortHighToLow.slice(0, 5).map((token) => {
+            return (
+                <li key={token.name + token.id}>
+                    {token.name} - {displayTimeDifference(token.holding_period)}</li>
+            )
+        })
 
 
 
-
-    const convertTime = (timestamp) => {
-        const num = Number(timestamp) * 1000
-        const date = new Date(num)
-
-        const formattedDate = (date.toLocaleString("en-US", { day: "numeric" }) + " " + date.toLocaleString("en-US", { month: "short" }) + " " + date.toLocaleString("en-US", { year: "numeric" }))
-        return (formattedDate)
-    }
 
 
     return (
         <div>
-            <h4>NFT holding period</h4>
+            <h4>Top 5 NFT holding period</h4>
+            <ul>
+                {topFiveHoldingPeriod}
+            </ul>
 
         </div>
     )
+
 }
 
 export default NftHoldingPeriod
